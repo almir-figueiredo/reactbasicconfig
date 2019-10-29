47323ba64d2abce97190c3c17bebc295f8094a08
ALMIR PICANCO DE FIGUEIREDO <56123856+almir-figueiredo@users.noreply.github.com>
2019-10-29 08:45:36 -0300

    Update routes.js

--- src/routes.js
+++ src/routes.js
@@ -1,1 +1,14 @@
-import { Switch, Route,  BrowserRouter } from 'react-router-dom';
+// rename routes as previously defined
+import { Switch, Route, BrowserRouter } from "react-router-dom";
+
+import route1 from ".pages/route1";
+import route2 from ".pages/route2";
+
+export default function Routes() {
+  return (
+    <Switch>
+      <Route path="/" exact component={route1} />
+      <Route path="/route2" exact component={route2} />
+    </Switch>
+  );
+}
