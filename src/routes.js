08084ff62e776fa675330f877a54560c3f8877c9
ALMIR PICANCO DE FIGUEIREDO <56123856+almir-figueiredo@users.noreply.github.com>
2019-10-29 08:53:59 -0300

    Update routes.js

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,6 @@
 // rename routes as previously defined
 import React from "react";
-import { Switch, Route, BrowserRouter } from "react-router-dom";
+import { Switch, Route } from "react-router-dom";
 
 import route1 from "./pages/route1";
 import route2 from "./pages/route2";
